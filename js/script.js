b622bb0ddc1ab60fb166efaafc99efa2896561bc
Naufal Athallah Iwel <spealdm6@gmail.com>
2023-09-17 14:51:45 +0700

    fix: separate script

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://api.nscreativestudio.com/api/portfolio")
+    .then((response) => response.json())
+    .then((data) => {
+      const gallery = document.getElementById("gallery");
+      data.forEach((item) => {
+        const imageUrl =
+          "https://api.nscreativestudio.com/assets/images/porto/" + item.gambar;
+        const card = `
+                    <div class="card">
+                        <img class="h-auto max-w-full rounded-lg" src="${imageUrl}" alt="${item.judul}">
+                        <div class="mt-2">
+                            <h3 class="text-xl font-bold">${item.judul}</h3>
+                            <p class="text-sm text-gray-600">${item.deskripsi}</p>
+                        </div>
+                    </div>
+                `;
+        gallery.insertAdjacentHTML("beforeend", card);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
